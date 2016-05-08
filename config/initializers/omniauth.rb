5b70658aae08600bac38a62a0f4e1aa798fbc6e3
Lucas Pavanelli <lucasapava@gmail.com>
2016-05-08 05:12:40 -0300

    Adding OmniAuth (2)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 'YOUR-APP-ID-HERE', 'YOUR-APP-SECRET-HERE'
+  provider :facebook, '480698852123692', '1d0d4e13ba522f747d2f60673c7a5559'
 end
