eda51b113d10abd624525ecc35e3938737a5186f
Miguel <miguelszcorti@gmail.com>
2016-05-14 00:40:56 -0300

    Facebook auth 1.5

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(env["omniauth.auth"], current_user)
+    user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
     redirect_to root_url
   end
