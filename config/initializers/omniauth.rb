d3e0ac28d05911919437fbc1e680412e759a84dd
Miguel <miguelszcorti@gmail.com>
2016-05-13 23:23:36 -0300

    Trying to fix facebook auth 1.0

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '480698852123692', '1d0d4e13ba522f747d2f60673c7a5559'
+  provider :facebook, ENV['FACEBOK_KEY'], ENV['FACEBOK_SECRET']
 end
