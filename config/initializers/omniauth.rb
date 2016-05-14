c741ea18972839ae1aa2ec97c74287174a02dcae
Miguel <miguelszcorti@gmail.com>
2016-05-14 00:20:27 -0300

    Facebook auth 1.3

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '480698852123692', '1d0d4e13ba522f747d2f60673c7a5559'
+  provider :facebook, '480698852123692', '1d0d4e13ba522f747d2f60673c7a5559', {:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 end
