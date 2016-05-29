2c3c3e93c98aa9519e389922395908ffaf331d40
Handel <hscholzem@gmail.com>
2016-05-29 03:57:11 -0300

    t

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,5 @@
+class StaticPagesController < ApplicationController
+ 
+  def sobre
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  #static pages
+  match '/sobre', to: 'static_pages#sobre', via: 'get'
+
+
+
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match '/signin',  to: 'sessions#new',         via: 'get'
