848e97badbfa046caf942f18c7d54bc0ae8d3b93
Miguel <miguelszcorti@gmail.com>
2016-06-09 23:20:03 -0300

    Dumb Error 1001

--- app/jobs/two_day_pressure_job.rb
+++ app/jobs/two_day_pressure_job.rb
@@ -2,7 +2,7 @@ class TwoDayPressureJob < Struct.new(:mobilization_id)
 	def perform
 		@mobilization = Mobilization.find(mobilization_id)
 		@mobilization.last_sent_email = DateTime.now
-        Delayed::Job.enqueue(TwoDayPressureJob.new(@mobilization.id), :run_at => (@mobilization.last_sent_email + 2.days)
+        Delayed::Job.enqueue(TwoDayPressureJob.new(@mobilization.id), :run_at => (@mobilization.last_sent_email + 2.days))
 		@mobilization.targets.each do |target|      
 	  		UserMailer.pressure_mail(@mobilization, target).deliver_later
 		end
