eb192a750974e2a3d68e0ec0f18a829b8fa9ffe0
Miguel <miguelszcorti@gmail.com>
2016-06-12 19:23:05 -0300

    Trying to make replying email possible

--- app/mailers/email_receiver.rb
+++ app/mailers/email_receiver.rb
@@ -5,10 +5,12 @@ class EmailReceiver
 
   def process
     mobTitle = @email.subject
-    #downSubject = @email.subject.downcase
-    #if (downSubject[0] + downSubject[1] ==  're')
-     # removeCharsFrom mobTitle, 4
-    #end
+    downSubject = @email.subject.downcase
+    if (downSubject[0] + downSubject[1] ==  're')
+      for i in 0..3 do
+        mobTitle[0] = ''
+      end
+    end
     mobilization = Mobilization.find_by_title(mobTitle)
     target = Target.find_by_email(@email.from[:email])
     mobilization.comments.create!(
