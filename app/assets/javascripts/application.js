f589802197cac1bcfbe3b41740d9319b2fab9e6c
Gabriel Vincent <gabriel@vincent.es>
2016-06-18 00:40:46 -0300

    Fixed card title bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require jquery_ujs
+//= require jquery.textfill.min
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require material
 //= require_tree .
